f6753b019c7dbc357a207dd9eabf56df896d7bd7
Ryan <ryan@Ryans-MacBook-4.local>
2026-09-15 12:11:28 -0700

    Database setup, setup tables, tested backend connection to db, created 5 tables for data

--- CSPC 491 Multimedia Recommendation website/backend/server.js
+++ CSPC 491 Multimedia Recommendation website/backend/server.js
@@ -8,13 +8,15 @@ const PORT = 3001
 app.use(cors())
 app.use(express.json())
 
+//test if backend is connected to frontend
 app.get("/api/test", (req, res) => {
   res.json({
     message: "Backend is working!"
   })
 })
 
-app.get("/api/db-test", async (req, res) => {
+//tests if able to connect to db
+/*app.get("/api/db-test", async (req, res) => {
   try {
     const result = await pool.query("SELECT NOW()")
 
@@ -27,6 +29,23 @@ app.get("/api/db-test", async (req, res) => {
 
     res.status(500).json({
       message: "Database connection failed"
+    })
+  }
+}) */
+
+//Tests database readings 
+app.get("/api/users", async (req, res) => {
+  try {
+    const result = await pool.query(
+      "SELECT id, name, email, account_state, created_at FROM users"
+    )
+
+    res.json(result.rows)
+  } catch (error) {
+    console.error(error)
+
+    res.status(500).json({
+      message: "Could not retrieve users"
     })
   }
 })
